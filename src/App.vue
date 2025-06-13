6c71710df6a3131e679893c1645d805634023abb
Aram993 <aram993@mail.ru>
2025-06-13 19:31:10 +0300

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
 import FirstComponent from './components/FirstComponent.vue';
-import Header from './components/Header.vue';
 
 </script>
 
 <template>
   <Header/>
-  <router-view/>
 </template>
 
 <style scoped>
--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,49 +1,11 @@
 <template>
     <div class="container">
-        <!-- <input type="text" @input="getLetters" :value="recipe"> -->
-        <input type="text" v-model="recipe">
+        FirstComponent
     </div>
 </template>
 <script setup>
-import { ref, watch } from 'vue';
-const recipe = ref("asd");
-import { useAxios } from '@/composables/useAxios';
-import { onMounted } from 'vue';
-import debounce from '@/utils/debounce';
-import recepieService from '@/services/recepieService';
-
-const {getRecepies} = recepieService();
-const {data, error, isLoading, refresh: getRecipes} = useAxios(() => getRecepies({q: recipe.value}));
-
-const debouncedGetRecepies = debounce(() => getRecipes(), 1000);
-
-// function getLetters(event) {
-//     recipe.value = event.target.value;
-//     params.value = {...params.value, q: recipe.value};
-//     // params.value = Object.assign(params.value,  {q: recipe.value})
-//     debouncedGetRecepies(); 
-  
-// }
-
-watch(recipe, (newVal) => {
-     debouncedGetRecepies();
-})
 
 </script>
 <style lang="scss">
-    .container {
-        margin: 200px;
-        display: flex;
-
-        & > input {
-            width: 500px;
-            height: 30px;
-            outline: none;
-        }
-
-        & > button {
-            width: 100px;
-            cursor: pointer;
-        }
-    }
+    
 </style>
